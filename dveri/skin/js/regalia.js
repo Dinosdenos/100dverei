0bcfcb36b3cfac3bdefdbc16c2adc7177d47ad39
Dinosdenos <165958306+Dinosdenos@users.noreply.github.com>
2024-08-24 03:31:13 +0300

    Update regalia.js

--- dveri/skin/js/regalia.js
+++ dveri/skin/js/regalia.js
@@ -294,6 +294,7 @@ $(".homepage-v7-categories-wrap").slick({
     dots: false,
     autoplay: false,
     autoplaySpeed: 6000,
+    touchMove: true,
     responsive: [{
             breakpoint: 1400,
             settings: {
